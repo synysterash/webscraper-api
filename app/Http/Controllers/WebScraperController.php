43c92c248ac67dbddd8df8ac7dfce9526046c984
Edris Ashman <ash@kestrl.io>
2024-05-09 01:54:08 +0800

    remove comments

--- app/Http/Controllers/WebScraperController.php
+++ app/Http/Controllers/WebScraperController.php
@@ -10,24 +10,19 @@ class WebScraperController extends Controller
 {
     public function scrape(Request $request)
     {
-        // Create a new browser instance
         $browser = new HttpBrowser(HttpClient::create());
 
-        // Get the URL from the query string and validate it
         $url = $request->query('url');
         if (filter_var($url, FILTER_VALIDATE_URL) === false) {
             return response()->json(['error' => 'Invalid URL'], 400);
         }
 
-        // Visit the URL
         $crawler = $browser->request('GET', $url);
 
-        // Example: Extract all links
         $links = $crawler->filter('a')->each(function ($node) {
             return $node->text().' - '.$node->link()->getUri();
         });
 
-        // Return the scraped data as JSON
         return response()->json(['links' => $links]);
     }
 }
